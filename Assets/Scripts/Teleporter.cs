7816f92aa38cf6bfb0cc42882084874130e6d230
frankrs <frankstapleton3d@gmail.com>
2015-07-24 16:14:12 -0400

    built for unity 5

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleporter : MonoBehaviour {
+
+	public GameObject[] teleports;
+
+	private int index = 0;
+
+	void Start(){
+		teleports = GameObject.FindGameObjectsWithTag ("Teleport");
+	}
+
+	void Update(){
+		if (Input.GetKeyDown ("space")) {
+			Teleport();
+		}
+	}
+
+	void Teleport(){
+		index ++;
+		if (index == teleports.Length) {
+			index = 0;
+		}
+		transform.position = teleports [index].transform.position;
+	}
+}
