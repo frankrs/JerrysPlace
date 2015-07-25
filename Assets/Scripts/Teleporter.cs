7cbfc7794425ecfc16f065d381e82cf1adf91275
frankrs <frankstapleton3d@gmail.com>
2015-07-25 16:21:44 -0400

    last commit before publishing on playstore

--- Assets/Scripts/Teleporter.cs
+++ Assets/Scripts/Teleporter.cs
@@ -3,19 +3,36 @@ using System.Collections;
 
 public class Teleporter : MonoBehaviour {
 
+
+
+//	void OnEnable(){
+//		//Cardboard.SDK.OnTrigger += Teleport;
+//	}
+//
+//	void OnDisable(){
+//		//Cardboard.SDK.OnTrigger -= Teleport;
+//	}
+	
 	public GameObject[] teleports;
+
+	public float teleportTime = 15f;
 
 	private int index = 0;
 
+
+
 	void Start(){
 		teleports = GameObject.FindGameObjectsWithTag ("Teleport");
+		InvokeRepeating ("Teleport", teleportTime, teleportTime);
 	}
 
-	void Update(){
-		if (Input.GetKeyDown ("space")) {
-			Teleport();
-		}
-	}
+//	void Update(){
+//		if (Input.GetKeyDown ("space")) {
+//			Teleport();
+//		}
+//	}
+
+
 
 	void Teleport(){
 		index ++;
@@ -23,5 +40,6 @@ public class Teleporter : MonoBehaviour {
 			index = 0;
 		}
 		transform.position = teleports [index].transform.position;
+		transform.rotation = teleports [index].transform.rotation;
 	}
 }
